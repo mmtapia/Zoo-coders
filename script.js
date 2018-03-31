d58a6f1beb2bca753b082a32d800905d2e95cf99
CTSDH <>
2018-03-31 12:02:23 -0500

    Added gif background

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log('hi');
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Animals in sanctuaries are treated better than in zoos. Instead of having the animals to sell tickets, they are there to have a safe place to live when they are recovering form an injury.";
+}
