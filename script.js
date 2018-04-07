85354f015267831c4a9fcc3e03b12ccfcfaaff76
Confer21 <Confer21@luc.edu>
2018-04-07 11:40:30 -0500

    nav bar + background

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 console.log('hi');
 
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Animals in sanctuaries are treated better than in zoos. Instead of having the animals to sell tickets, they are there to have a safe place to live when they are recovering form an injury.";
-}
+
